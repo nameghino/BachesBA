9b111fab14cf152067ba1f9e4e84b03f13bfbfba
Nicolas Ameghino <nameghino@coto.com.ar>
2013-01-29 11:56:18 -0300

    Initial Commit

---
+++ BachesBA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BachesBA
+//
+//  Created by Nicolas Ameghino on 29/01/13.
+//  Copyright (c) 2013 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
